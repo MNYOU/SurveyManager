030858311aed68d3557c74fe72b613c8606b6a01
Arman Mukanov <mukanovarman777@gmail.com>
2023-11-26 23:36:16 +0500

    Infrastructure

--- Domain/Entities/Base/Entity.cs
+++ Domain/Entities/Base/Entity.cs
@@ -1,8 +1,10 @@
-﻿namespace Domain.Entities;
+﻿namespace Domain.Entities.Base;
 
 // TODO привязать dateCreated, dateUpdated и т.д. в сущности
 // TODO auditHistory не работает
 public abstract class Entity
 {
+    public Guid Id { get; set; }
     
+    // либо сюда добавить все это
 }
