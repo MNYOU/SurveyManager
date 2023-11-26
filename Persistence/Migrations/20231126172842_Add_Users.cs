8e213eb45dfbc84ba7d72e49eab71954321d3978
Arman Mukanov <mukanovarman777@gmail.com>
2023-11-26 23:35:06 +0500

    Добавил миграцию

---
+++ Persistence/Migrations/20231126172842_Add_Users.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "SurveyManager");
+
+            migrationBuilder.CreateTable(
+                name: "AuditHistory",
+                schema: "SurveyManager",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    RowId = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    TableName = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    Changed = table.Column<string>(type: "text", nullable: false),
+                    Kind = table.Column<int>(type: "integer", nullable: false),
+                    Created = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    Username = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AuditHistory", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                schema: "SurveyManager",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Role = table.Column<int>(type: "integer", nullable: false),
+                    Login = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    EmailIsConfirmed = table.Column<bool>(type: "boolean", nullable: false),
+                    PasswordHash = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Survey",
+                schema: "SurveyManager",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    CreationTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    AdminId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Survey", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Survey_users_AdminId",
+                        column: x => x.AdminId,
+                        principalSchema: "SurveyManager",
+                        principalTable: "users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Question",
+                schema: "SurveyManager",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    SurveyId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Type = table.Column<int>(type: "integer", nullable: false),
+                    SequenceNumber = table.Column<int>(type: "integer", nullable: false),
+                    MinValue = table.Column<int>(type: "integer", nullable: true),
+                    MaxValue = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Question", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Question_Survey_SurveyId",
+                        column: x => x.SurveyId,
+                        principalSchema: "SurveyManager",
+                        principalTable: "Survey",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PatientAnswer",
+                schema: "SurveyManager",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    QuestionId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    PatientFIO = table.Column<string>(type: "text", nullable: false),
+                    TextAnswer = table.Column<string>(type: "text", nullable: true),
+                    RangeAnswer = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PatientAnswer", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PatientAnswer_Question_QuestionId",
+                        column: x => x.QuestionId,
+                        principalSchema: "SurveyManager",
+                        principalTable: "Question",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AnswerOption",
+                schema: "SurveyManager",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    QuestionId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Answer = table.Column<string>(type: "text", nullable: false),
+                    PatientAnswerId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AnswerOption", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AnswerOption_PatientAnswer_PatientAnswerId",
+                        column: x => x.PatientAnswerId,
+                        principalSchema: "SurveyManager",
+                        principalTable: "PatientAnswer",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_AnswerOption_Question_QuestionId",
+                        column: x => x.QuestionId,
+                        principalSchema: "SurveyManager",
+                        principalTable: "Question",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AnswerOption_PatientAnswerId",
+                schema: "SurveyManager",
+                table: "AnswerOption",
+                column: "PatientAnswerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AnswerOption_QuestionId",
+                schema: "SurveyManager",
+                table: "AnswerOption",
+                column: "QuestionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PatientAnswer_QuestionId",
+                schema: "SurveyManager",
+                table: "PatientAnswer",
+                column: "QuestionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Question_SurveyId",
+                schema: "SurveyManager",
+                table: "Question",
+                column: "SurveyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Survey_AdminId",
+                schema: "SurveyManager",
+                table: "Survey",
+                column: "AdminId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_Email",
+                schema: "SurveyManager",
+                table: "users",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_Login",
+                schema: "SurveyManager",
+                table: "users",
+                column: "Login",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AnswerOption",
+                schema: "SurveyManager");
+
+            migrationBuilder.DropTable(
+                name: "AuditHistory",
+                schema: "SurveyManager");
+
+            migrationBuilder.DropTable(
+                name: "PatientAnswer",
+                schema: "SurveyManager");
+
+            migrationBuilder.DropTable(
+                name: "Question",
+                schema: "SurveyManager");
+
+            migrationBuilder.DropTable(
+                name: "Survey",
+                schema: "SurveyManager");
+
+            migrationBuilder.DropTable(
+                name: "users",
+                schema: "SurveyManager");
+        }
+    }
+}
