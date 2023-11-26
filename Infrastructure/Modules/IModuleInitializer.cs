bf28ac78d21142ec6600f3c2e58b297c86fca356
Arman Mukanov <mukanovarman777@gmail.com>
2023-11-26 23:45:21 +0500

    Рефакторинг

---
+++ Infrastructure/Modules/IModuleInitializer.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Infrastructure.Modules;
+
+public interface IModuleInitializer
+{
+    // TODO вынести реализацию из стартапа в класс, реализующий это 
+    void ConfigureServices(IServiceCollection services, IConfiguration configuration);
+}
