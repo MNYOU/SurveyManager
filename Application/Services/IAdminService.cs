ea3e7182ee5a41f3344a07d31c78d377a013e65e
Arman Mukanov <mukanovarman777@gmail.com>
2023-11-28 23:57:07 +0500

    добавил новый сервис

---
+++ Application/Services/IAdminService.cs
@@ -0,0 +1,9 @@
+﻿using Application.Models.Responses.Admin;
+using Infrastructure.Common.Result;
+
+namespace Application.Services;
+
+public interface IAdminService
+{
+    public Task<Result<AccessKey>> GetAccessCode(Guid adminId);
+}
---
+++ Application/Services/ISurveyService.cs
@@ -0,0 +1,20 @@
+﻿using Application.Models.Requests.Survey;
+using Application.Models.Responses.Survey;
+using Infrastructure.Common.Result;
+
+namespace Application.Services;
+
+
+// TODO отделельные модели для прохождения и для просмотра(админом, аналитиком)
+public interface ISurveyService
+{
+    public Task<Result> CreateAsync(Guid adminId, CreateSurveyRequest request);
+
+    public Result Update(Guid adminId, Guid surveyId, CreateSurveyRequest request);
+
+    public Result Delete(DeleteSurveyRequest request);
+    
+    public Result<SurveyView> GetSurveyResult(Guid id);
+
+    public Result<IEnumerable<SurveyPreview>> GetSurveysPreviewByAdmin(Guid adminId);
+}
