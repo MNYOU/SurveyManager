937c969e7ed215f18038e92160b3c595c7ac1ba3
Arman Mukanov <mukanovarman777@gmail.com>
2023-11-28 23:58:08 +0500

    Добавил моковый сервис

---
+++ Application/Services/AdminService.cs
@@ -0,0 +1,12 @@
+﻿using Application.Models.Responses.Admin;
+using Infrastructure.Common.Result;
+
+namespace Application.Services;
+
+public class AdminService: IAdminService
+{
+    public async Task<Result<AccessKey>> GetAccessCode(Guid adminId)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Application/Services/SurveyService.cs
@@ -0,0 +1,40 @@
+﻿using Application.Models.Requests.Survey;
+using Application.Models.Responses.Survey;
+using Infrastructure.Common.Result;
+
+namespace Application.Services;
+
+public class SurveyService: ISurveyService
+{
+    public async Task<Result> CreateAsync(Guid adminId, CreateSurveyRequest request)
+    {
+        await AddDefaultQuestions(request);
+        throw new NotImplementedException();
+    }
+    
+    private async Task AddDefaultQuestions(CreateSurveyRequest request)
+    {
+        throw new NotImplementedException();
+        // TODO добавить 2 дефолтных вопроса
+    }
+
+    public Result Update(Guid adminId, Guid surveyId, CreateSurveyRequest request)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Result Delete(DeleteSurveyRequest request)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Result<SurveyView> GetSurveyResult(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Result<IEnumerable<SurveyPreview>> GetSurveysPreviewByAdmin(Guid adminId)
+    {
+        throw new NotImplementedException();
+    }
+}
