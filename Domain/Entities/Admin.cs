3dcf58e3e263e40beaa9bc337c060e4e2a81dd01
Arman Mukanov <mukanovarman777@gmail.com>
2023-12-03 18:32:26 +0500

    Добавил новую сущность

---
+++ Domain/Entities/Admin.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Entities.Base;
+
+namespace Domain.Entities;
+
+public class Admin: Entity
+{
+    public Guid AccessKey { get; set; }
+
+    public ICollection<Survey> Surveys { get; set; }
+    
+}
